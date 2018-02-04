0d280015985b82080d0ec7667d974a3bd63b9dea
Tacuma Bell <takumab@gmail.com>
2018-02-04 22:50:32 +0000

    Initial commit

---
+++ sorting_and_searching/merge_sort.py
@@ -0,0 +1,7 @@
+# Merge sort is recursive algorithm that continually splits a list in half.
+# If the list is empty or has one item, it is sorted already.
+# If list has more than item we split the list in half.
+# Then recursively invoke/call merge sort on both halves.
+# Once two halves are sorted, we call merge.
+# Merge takes two smaller sorted list and combines them together in a single,
+# sorted, new list.
---
+++ sorting_and_searching/selection_sort.py
@@ -0,0 +1,57 @@
+# selection_sort :: unsorted_list -> sorted_list
+def selection_sort(unsorted_list):
+    """Takes an unsorted_list and returns a sorted list
+    >>> selection_sort([3, 2, 1])
+    [1, 2, 3]
+    """
+    sorted_list = []
+    while len(unsorted_list) != 0:
+        # find_smallest number in list
+        smallest = find_smallest(unsorted_list)
+        # move smallest number to sorted
+        move_smallest_to_sorted(sorted_list, smallest)
+        remove_unsorted_element(unsorted_list, smallest)
+    return sorted_list
+
+# find_smallest :: List -> Integer
+def find_smallest(xs):
+    """Takes a list of numbers and returns the smallest number
+    >>> find_smallest([3, 2, 1])
+    1
+    """
+    acc = xs[0]
+    smallest = None
+    for x in range(0, len(xs)):
+        if xs[x] > acc:
+            smallest = acc
+        else:
+            smallest = xs[x]
+            acc = smallest
+    # ...n
+    return acc
+
+
+# move_smallest_to_sorted :: smallest_num --> sorted_list
+def move_smallest_to_sorted(sorted, smallest):
+    """Takes smallest number from find_smallest() and moves it to the sorted_list
+    >>> move_smallest_to_sorted([1, 2], 3)
+    [1, 2, 3]
+    """
+    sorted.append(smallest)
+    return sorted
+
+
+# remove_unsorted_list :: (List, Integer) --> List
+def remove_unsorted_element(original_unsorted_list, smallest_num):
+    """Remove unsorted element from list
+    >>> remove_unsorted_element([3,2,1], 1)
+    [3, 2]
+    """
+    smallest_index = original_unsorted_list.index(smallest_num)
+    original_unsorted_list.pop(smallest_index)
+    return original_unsorted_list
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    print(selection_sort([5, 7, 2, 6, 8, 4, 1, 3, 10, 0, 25]))
