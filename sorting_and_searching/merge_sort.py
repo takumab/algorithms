f58d4d1868c4de475698d62e054d78759aa3f926
Tacuma Bell <takumab@gmail.com>
2018-02-05 19:33:38 +0000

    Added doctest

--- sorting_and_searching/merge_sort.py
+++ sorting_and_searching/merge_sort.py
@@ -5,3 +5,24 @@
 # Once two halves are sorted, we call merge.
 # Merge takes two smaller sorted list and combines them together in a single,
 # sorted, new list.
+
+
+# merge :: (List1, List2) --> merged_sorted_list
+def merge(alist, blist):
+    """
+    Takes two sorted list and returns a merged_sorted_list
+    :param alist:
+    :param blist:
+    :return: merged_sorted_list
+
+    >>> merge([3, 4, 5], [6, 7, 8])
+    [3, 4, 5, 6, 7, 8]
+    """
+    # .... [3, 4, 5, 6, 7, 8]
+    return 0
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
