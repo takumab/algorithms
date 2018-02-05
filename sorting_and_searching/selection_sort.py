68f9dc25baaa44dce0df82f8f94d78f16b7f9cc3
Tacuma Bell <takumab@gmail.com>
2018-02-05 19:01:31 +0000

    Added Big-O runtime

--- sorting_and_searching/selection_sort.py
+++ sorting_and_searching/selection_sort.py
@@ -6,6 +6,8 @@ Selection Sort:
         Repeat until no unsorted elements remain:
             --> Search the unsorted part of the data to find the smallest value
             --> Swap the smallest found value with the first element of the unsorted part
+
+    Big-O == O(n^2)
 """
 
 
